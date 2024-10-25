3ea2fe79d600e61eb16fa1191e965d106ff35f1a
Damilaredavid <damilare.damsy@gmail.com>
2024-10-25 12:57:05 +0100

    Initial commit

---
+++ WoWonder/Helpers/Chat/BroadcastMessageController.cs
@@ -0,0 +1,129 @@
+﻿using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WoWonder.Activities.Chat.Broadcast;
+using WoWonder.Activities.Chat.MsgTabbes;
+using WoWonder.Helpers.Chat.Jobs;
+using WoWonder.Helpers.Controller;
+using WoWonder.Helpers.Model;
+using WoWonder.Helpers.Utils;
+using WoWonder.SQLite;
+using WoWonderClient.Classes.Message;
+using WoWonderClient.JobWorker;
+using WoWonderClient.Requests;
+
+namespace WoWonder.Helpers.Chat
+{
+    public class BroadcastMessageController
+    {
+        //############# DONT'T MODIFY HERE ############# 
+        private static BroadcastChatWindowActivity MainWindowActivity;
+        private static ChatTabbedMainActivity GlobalContext;
+
+        //========================= Functions ========================= 
+        public static async Task SendMessageTask(BroadcastChatWindowActivity windowActivity, string id, string messageId, string text = "", string contact = "", string pathFile = "", string imageUrl = "", string stickerId = "", string gifUrl = "", string lat = "", string lng = "", string replyId = "")
+        {
+            try
+            {
+                MainWindowActivity = windowActivity;
+                GlobalContext = ChatTabbedMainActivity.GetInstance();
+
+                if (!string.IsNullOrEmpty(pathFile))
+                {
+                    new UploadSingleFileToServerWorker(windowActivity, "BroadcastChatWindowActivity").UploadFileToServer(windowActivity, new FileModel
+                    {
+                        MessageHashId = messageId,
+                        BroadcastId = id,
+                        FilePath = pathFile,
+                        ReplyId = replyId,
+                    });
+                }
+                else
+                {
+                    StartApiService(id, messageId, text, contact, pathFile, imageUrl, stickerId, gifUrl, lat, lng, replyId);
+                }
+            }
+            catch (Exception ex)
+            {
+                await Task.CompletedTask;
+                Methods.DisplayReportResultTrack(ex);
+            }
+        }
+
+        private static void StartApiService(string id, string messageId, string text = "", string contact = "", string pathFile = "", string imageUrl = "", string stickerId = "", string gifUrl = "", string lat = "", string lng = "", string replyId = "")
+        {
+            if (!Methods.CheckConnectivity())
+                ToastUtils.ShowToast(MainWindowActivity, MainWindowActivity.GetString(Resource.String.Lbl_CheckYourInternetConnection), ToastLength.Short);
+            else
+                PollyController.RunRetryPolicyFunction(new List<Func<Task>> { () => SendMessage(id, messageId, text, contact, pathFile, imageUrl, stickerId, gifUrl, lat, lng, replyId) });
+        }
+
+        private static async Task SendMessage(string id, string messageId, string text = "", string contact = "", string pathFile = "", string imageUrl = "", string stickerId = "", string gifUrl = "", string lat = "", string lng = "", string replyId = "")
+        {
+            var (apiStatus, respond) = await RequestsAsync.Broadcast.SendBroadcastMessageAsync(id, messageId, text, contact, pathFile, imageUrl, stickerId, gifUrl, lat, lng, replyId);
+            if (apiStatus == 200)
+            {
+                if (respond is SendMessageObject result)
+                {
+                    UpdateLastIdMessage(result);
+                }
+            }
+            else Methods.DisplayReportResult(MainWindowActivity, respond);
+        }
+
+        public static void UpdateLastIdMessage(SendMessageObject chatMessages)
+        {
+            try
+            {
+                MessageData messageInfo = chatMessages?.MessageData?.FirstOrDefault();
+                if (messageInfo != null)
+                {
+                    var typeModel = ChatTools.GetTypeModel(messageInfo);
+                    if (typeModel == MessageModelType.None)
+                        return;
+
+                    AdapterModelsClassMessage checker = MainWindowActivity?.MAdapter?.DifferList?.FirstOrDefault(a => a.MesData?.Id == messageInfo.MessageHashId);
+                    if (checker != null)
+                    {
+                        var message = ChatTools.MessageFilter(messageInfo.ToId, messageInfo, typeModel, true);
+                        message.ModelType = typeModel;
+                        message.ErrorSendMessage = false;
+                        message.Seen ??= "0";
+                        message.BtnDownload = true;
+
+                        checker.MesData = message;
+                        checker.Id = Java.Lang.Long.ParseLong(message.Id);
+                        checker.TypeView = typeModel;
+
+                        //Update All data users to database
+                        SqLiteDatabase dbDatabase = new SqLiteDatabase();
+                        dbDatabase.Insert_Or_Update_To_one_BroadcastMessagesTable(checker.MesData);
+
+                        MainWindowActivity?.RunOnUiThread(() =>
+                        {
+                            try
+                            {
+                                //Update data RecyclerView Messages.
+                                //if (message.ModelType == MessageModelType.RightSticker || message.ModelType == MessageModelType.RightImage || message.ModelType == MessageModelType.RightMap || message.ModelType == MessageModelType.RightVideo)
+                                MainWindowActivity?.UpdateOneMessage(checker.MesData);
+
+                                if (UserDetails.SoundControl)
+                                    Methods.AudioRecorderAndPlayer.PlayAudioFromAsset("Popup_SendMesseges.mp3");
+                            }
+                            catch (Exception e)
+                            {
+                                Methods.DisplayReportResultTrack(e);
+                            }
+                        });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Methods.DisplayReportResultTrack(e);
+            }
+        }
+    }
+}
